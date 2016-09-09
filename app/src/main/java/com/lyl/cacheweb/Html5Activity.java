dd4cb7f825d4c1fe2aac827b29e661d0a098c6cd
lyl <iamlyl@foxmail.com>
2016-09-09 17:33:33 +0800

    双击返回键回到首次输入的网址

--- app/src/main/java/com/lyl/cacheweb/Html5Activity.java
+++ app/src/main/java/com/lyl/cacheweb/Html5Activity.java
@@ -1,18 +1,11 @@
 package com.lyl.cacheweb;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.webkit.GeolocationPermissions;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
 
 public class Html5Activity extends AppCompatActivity {
@@ -31,7 +24,7 @@ public class Html5Activity extends AppCompatActivity {
         Bundle bundle = getIntent().getBundleExtra("bundle");
         if (bundle != null) {
             mUrl = bundle.getString("url");
-        }else {
+        } else {
             mUrl = "https://wing-li.github.io/";
         }
 
@@ -39,114 +32,25 @@ public class Html5Activity extends AppCompatActivity {
 
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams
                 .MATCH_PARENT);
-        mWebView = new WebView(getApplicationContext());
+        mWebView = new Html5WebView(getApplicationContext());
         mWebView.setLayoutParams(params);
         mLayout.addView(mWebView);
 
-        WebSettings mWebSettings = mWebView.getSettings();
-        mWebSettings.setSupportZoom(true);
-        mWebSettings.setLoadWithOverviewMode(true);
-        mWebSettings.setUseWideViewPort(true);
-        mWebSettings.setDefaultTextEncodingName("utf-8");
-        mWebSettings.setLoadsImagesAutomatically(true);
-
-
-        //调用JS方法.安卓版本大于17,加上注解 @JavascriptInterface
-        mWebSettings.setJavaScriptEnabled(true);
-        mWebSettings.setSupportMultipleWindows(true);
-
-        //缓存数据
-        saveData(mWebSettings);
-
-        newWin(mWebSettings);
-
-        mWebView.setWebChromeClient(webChromeClient);
-        mWebView.setWebViewClient(webViewClient);
         mWebView.loadUrl(mUrl);
     }
 
-    /**
-     * 多窗口的问题
-     */
-    private void newWin(WebSettings mWebSettings) {
-        //html中的_bank标签就是新建窗口打开，有时会打不开，需要加以下
-        //然后 复写 WebChromeClient的onCreateWindow方法
-        mWebSettings.setSupportMultipleWindows(true);
-        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-    }
-
-    /**
-     * HTML5数据存储
-     */
-    private void saveData(WebSettings mWebSettings) {
-        //有时候网页需要自己保存一些关键数据,Android WebView 需要自己设置
-
-        if (NetStatusUtil.isConnected(getApplicationContext())) {
-            mWebSettings.setCacheMode(WebSettings.LOAD_DEFAULT);//根据cache-control决定是否从网络上取数据。
-        } else {
-            mWebSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);//没网，则从本地获取，即离线加载
-        }
-
-        mWebSettings.setDomStorageEnabled(true);
-        mWebSettings.setDatabaseEnabled(true);
-        mWebSettings.setAppCacheEnabled(true);
-        String appCachePath = getApplicationContext().getCacheDir().getAbsolutePath();
-        mWebSettings.setAppCachePath(appCachePath);
-    }
-
-    WebViewClient webViewClient = new WebViewClient() {
-        /**
-         * 多页面在同一个WebView中打开，就是不新建activity或者调用系统浏览器打开
-         */
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            Log.d("Url:", url);
-            return true;
-        }
-    };
-
-    WebChromeClient webChromeClient = new WebChromeClient() {
-
-        //=========HTML5定位==========================================================
-        //需要先加入权限
-        //<uses-permission android:name="android.permission.INTERNET"/>
-        //<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>
-        //<uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"/>
-        @Override
-        public void onReceivedIcon(WebView view, Bitmap icon) {
-            super.onReceivedIcon(view, icon);
-        }
-
-        @Override
-        public void onGeolocationPermissionsHidePrompt() {
-            super.onGeolocationPermissionsHidePrompt();
-        }
-
-        @Override
-        public void onGeolocationPermissionsShowPrompt(final String origin, final GeolocationPermissions.Callback callback) {
-            callback.invoke(origin, true, false);//注意个函数，第二个参数就是是否同意定位权限，第三个是是否希望内核记住
-            super.onGeolocationPermissionsShowPrompt(origin, callback);
-        }
-        //=========HTML5定位==========================================================
-
-
-        //=========多窗口的问题==========================================================
-        @Override
-        public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
-            WebView.HitTestResult result = view.getHitTestResult();
-            String data = result.getExtra();
-            mWebView.loadUrl(data);
-            return true;
-        }
-        //=========多窗口的问题==========================================================
-    };
-
+    private long mOldTime;
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK && mWebView.canGoBack()) {
-            mWebView.goBack();
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (System.currentTimeMillis() - mOldTime < 1500) {
+                mWebView.clearHistory();
+                mWebView.loadUrl(mUrl);
+            } else if (mWebView.canGoBack()) {
+                mWebView.goBack();
+            }
+            mOldTime = System.currentTimeMillis();
             return true;
         }
 
---
+++ app/src/main/java/com/lyl/cacheweb/Html5WebView.java
@@ -0,0 +1,130 @@
+package com.lyl.cacheweb;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Message;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Wing_Li
+ * 2016/9/9.
+ */
+public class Html5WebView extends WebView {
+
+    private Context mContext;
+
+    public Html5WebView(Context context) {
+        super(context);
+        mContext = context;
+        init();
+    }
+
+    public Html5WebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Html5WebView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void init() {
+        WebSettings mWebSettings = getSettings();
+        mWebSettings.setSupportZoom(true);
+        mWebSettings.setLoadWithOverviewMode(true);
+        mWebSettings.setUseWideViewPort(true);
+        mWebSettings.setDefaultTextEncodingName("utf-8");
+        mWebSettings.setLoadsImagesAutomatically(true);
+
+        //调用JS方法.安卓版本大于17,加上注解 @JavascriptInterface
+        mWebSettings.setJavaScriptEnabled(true);
+        mWebSettings.setSupportMultipleWindows(true);
+        //缓存数据
+        saveData(mWebSettings);
+        newWin(mWebSettings);
+        setWebChromeClient(webChromeClient);
+        setWebViewClient(webViewClient);
+    }
+
+    /**
+     * 多窗口的问题
+     */
+    private void newWin(WebSettings mWebSettings) {
+        //html中的_bank标签就是新建窗口打开，有时会打不开，需要加以下
+        //然后 复写 WebChromeClient的onCreateWindow方法
+        mWebSettings.setSupportMultipleWindows(true);
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+    }
+
+    /**
+     * HTML5数据存储
+     */
+    private void saveData(WebSettings mWebSettings) {
+        //有时候网页需要自己保存一些关键数据,Android WebView 需要自己设置
+
+        if (NetStatusUtil.isConnected(mContext)) {
+            mWebSettings.setCacheMode(WebSettings.LOAD_DEFAULT);//根据cache-control决定是否从网络上取数据。
+        } else {
+            mWebSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);//没网，则从本地获取，即离线加载
+        }
+
+        mWebSettings.setDomStorageEnabled(true);
+        mWebSettings.setDatabaseEnabled(true);
+        mWebSettings.setAppCacheEnabled(true);
+        String appCachePath = mContext.getCacheDir().getAbsolutePath();
+        mWebSettings.setAppCachePath(appCachePath);
+    }
+
+    WebViewClient webViewClient = new WebViewClient() {
+        /**
+         * 多页面在同一个WebView中打开，就是不新建activity或者调用系统浏览器打开
+         */
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            Log.d("Url:", url);
+            return true;
+        }
+    };
+
+    WebChromeClient webChromeClient = new WebChromeClient() {
+
+        //=========HTML5定位==========================================================
+        //需要先加入权限
+        //<uses-permission android:name="android.permission.INTERNET"/>
+        //<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>
+        //<uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"/>
+        @Override
+        public void onReceivedIcon(WebView view, Bitmap icon) {
+            super.onReceivedIcon(view, icon);
+        }
+
+        @Override
+        public void onGeolocationPermissionsHidePrompt() {
+            super.onGeolocationPermissionsHidePrompt();
+        }
+
+        @Override
+        public void onGeolocationPermissionsShowPrompt(final String origin, final GeolocationPermissions.Callback callback) {
+            callback.invoke(origin, true, false);//注意个函数，第二个参数就是是否同意定位权限，第三个是是否希望内核记住
+            super.onGeolocationPermissionsShowPrompt(origin, callback);
+        }
+        //=========HTML5定位==========================================================
+
+
+        //=========多窗口的问题==========================================================
+        @Override
+        public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
+            WebView.HitTestResult result = view.getHitTestResult();
+            String data = result.getExtra();
+            view.loadUrl(data);
+            return true;
+        }
+        //=========多窗口的问题==========================================================
+    };
+}
